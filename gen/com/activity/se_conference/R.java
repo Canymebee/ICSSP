4acc6adf6c77a6250a8d0eb18ed62c87c5cb1f5a
Canymebee <1206968541@qq.com>
2013-12-22 14:32:56 +0800

    按drawer可以改标题栏颜色

--- gen/com/activity/se_conference/R.java
+++ gen/com/activity/se_conference/R.java
@@ -12,15 +12,17 @@ public final class R {
     }
     public static final class color {
         public static final int Blue_Color=0x7f040002;
+        /**  我现在下划线用的bluecolor,其实就是lightblue 
+         */
         public static final int Dark_Blue=0x7f040003;
         public static final int Dark_Grey=0x7f040000;
         public static final int Light_Grey=0x7f040001;
         /** 蜜色 
          */
-        public static final int aliceblue=0x7f040030;
+        public static final int aliceblue=0x7f040033;
         /** 亚麻色 
          */
-        public static final int antiquewhite=0x7f040026;
+        public static final int antiquewhite=0x7f040029;
         /** 中灰兰色 
          */
         public static final int aqua=0x7f040086;
@@ -29,31 +31,31 @@ public final class R {
         public static final int aquamarine=0x7f040067;
         /** 沙褐色
          */
-        public static final int azure=0x7f04002e;
+        public static final int azure=0x7f040031;
         /** 烟白色 
          */
-        public static final int beige=0x7f04002b;
+        public static final int beige=0x7f04002e;
         /** 浅玫瑰色 
          */
-        public static final int bisque=0x7f040011;
+        public static final int bisque=0x7f040014;
         /** 海军色 
          */
         public static final int black=0x7f040095;
         /** 番木色 
          */
-        public static final int blanchedalmond=0x7f04000f;
+        public static final int blanchedalmond=0x7f040012;
         /** 暗绿色 
          */
         public static final int blue=0x7f040091;
         /** 暗红色 
          */
-        public static final int blueviolet=0x7f04005f;
+        public static final int blueviolet=0x7f040061;
         /** 暗灰色 
          */
-        public static final int brown=0x7f040054;
+        public static final int brown=0x7f040056;
         /** 亮青色 
          */
-        public static final int burlywood=0x7f040038;
+        public static final int burlywood=0x7f04003b;
         /** 菊兰色 
          */
         public static final int cadetblue=0x7f040075;
@@ -62,19 +64,19 @@ public final class R {
         public static final int chartreuse=0x7f040068;
         /** 茶色 
          */
-        public static final int chocolate=0x7f040043;
+        public static final int chocolate=0x7f040046;
         /** 暗桔黄色 
          */
-        public static final int coral=0x7f04001b;
+        public static final int coral=0x7f04001e;
         /** 中绿色 
          */
         public static final int cornflowerblue=0x7f040074;
         /** 柠檬绸色 
          */
-        public static final int cornsilk=0x7f04000b;
+        public static final int cornsilk=0x7f04000e;
         /** 淡灰色 
          */
-        public static final int crimson=0x7f04003b;
+        public static final int crimson=0x7f04003e;
         /** 浅绿色 
          */
         public static final int cyan=0x7f040087;
@@ -86,40 +88,40 @@ public final class R {
         public static final int darkcyan=0x7f04008d;
         /** 中粉紫色 
          */
-        public static final int darkgoldenrod=0x7f04004b;
-        /** 亮蓝色 
+        public static final int darkgoldenrod=0x7f04004e;
+        /** 黄绿色 
          */
-        public static final int darkgray=0x7f040052;
+        public static final int darkgray=0x7f040054;
         /** 绿色 
          */
         public static final int darkgreen=0x7f040090;
         /** 暗灰色 
          */
-        public static final int darkgrey=0x7f040053;
+        public static final int darkgrey=0x7f040055;
         /** 银色 
          */
-        public static final int darkkhaki=0x7f040048;
+        public static final int darkkhaki=0x7f04004b;
         /** 重褐色 
          */
-        public static final int darkmagenta=0x7f04005d;
+        public static final int darkmagenta=0x7f04005f;
         /** 军兰色 
          */
         public static final int darkolivegreen=0x7f040076;
         /** 亮肉色 
          */
-        public static final int darkorange=0x7f04001a;
+        public static final int darkorange=0x7f04001d;
         /** 赭色 
          */
-        public static final int darkorchid=0x7f040056;
+        public static final int darkorchid=0x7f040058;
         /** 暗洋红 
          */
-        public static final int darkred=0x7f04005e;
+        public static final int darkred=0x7f040060;
         /** 紫罗兰色 
          */
-        public static final int darksalmon=0x7f040035;
+        public static final int darksalmon=0x7f040038;
         /** 亮绿色 
          */
-        public static final int darkseagreen=0x7f04005b;
+        public static final int darkseagreen=0x7f04005d;
         /** 中绿宝石 
          */
         public static final int darkslateblue=0x7f040079;
@@ -134,10 +136,10 @@ public final class R {
         public static final int darkturquoise=0x7f04008b;
         /** 苍绿色 
          */
-        public static final int darkviolet=0x7f040058;
+        public static final int darkviolet=0x7f04005a;
         /** 红橙色 
          */
-        public static final int deeppink=0x7f04001f;
+        public static final int deeppink=0x7f040022;
         /** 暗宝石绿 
          */
         public static final int deepskyblue=0x7f04008c;
@@ -152,29 +154,29 @@ public final class R {
         public static final int dodgerblue=0x7f040084;
         /** 暗金黄色 
          */
-        public static final int firebrick=0x7f04004c;
+        public static final int firebrick=0x7f04004f;
         /** 雪白色 
          */
-        public static final int floralwhite=0x7f040009;
+        public static final int floralwhite=0x7f04000c;
         /** 海绿色 
          */
         public static final int forestgreen=0x7f040082;
         /** 深粉红色 
          */
-        public static final int fuchsia=0x7f040020;
+        public static final int fuchsia=0x7f040023;
         /** 洋李色 
          */
-        public static final int gainsboro=0x7f04003a;
+        public static final int gainsboro=0x7f04003d;
         /** 鲜肉色 
          */
-        public static final int ghostwhite=0x7f040028;
+        public static final int ghostwhite=0x7f04002b;
         /** 桃色 
          */
-        public static final int gold=0x7f040015;
+        public static final int gold=0x7f040018;
         /** 苍紫罗兰色 
          */
-        public static final int goldenrod=0x7f04003d;
-        /** 天蓝色 
+        public static final int goldenrod=0x7f040040;
+        /** 紫罗兰蓝色 
          */
         public static final int gray=0x7f040062;
         /** 水鸭色 
@@ -182,73 +184,71 @@ public final class R {
         public static final int green=0x7f04008f;
         /** 苍宝石绿 
          */
-        public static final int greenyellow=0x7f040050;
+        public static final int greenyellow=0x7f040053;
         /** 灰色 
          */
         public static final int grey=0x7f040063;
         /** 天蓝色 
          */
-        public static final int honeydew=0x7f04002f;
+        public static final int honeydew=0x7f040032;
         /** 珊瑚色 
          */
-        public static final int hotpink=0x7f04001c;
+        public static final int hotpink=0x7f04001f;
         /** 秘鲁色 
          */
-        public static final int indianred=0x7f040045;
+        public static final int indianred=0x7f040048;
         /** 暗橄榄绿  
          */
         public static final int indigo=0x7f040077;
         /** 白色 
          */
-        public static final int ivory=0x7f040005;
+        public static final int ivory=0x7f040008;
         /** 艾利斯兰 
          */
-        public static final int khaki=0x7f040031;
+        public static final int khaki=0x7f040034;
         /** 暗肉色 
          */
-        public static final int lavender=0x7f040036;
+        public static final int lavender=0x7f040039;
         /** 海贝色 
          */
-        public static final int lavenderblush=0x7f04000d;
+        public static final int lavenderblush=0x7f040010;
         /** 黄绿色 
          */
         public static final int lawngreen=0x7f040069;
         /** 花白色 
          */
-        public static final int lemonchiffon=0x7f04000a;
-        /** 黄绿色 
+        public static final int lemonchiffon=0x7f04000d;
+        /** 亮天蓝色 
          */
-        public static final int lightblue=0x7f040051;
+        public static final int lightblue=0x7f040005;
         /** 黄褐色 
          */
-        public static final int lightcoral=0x7f040032;
+        public static final int lightcoral=0x7f040035;
         /** 淡紫色 
          */
-        public static final int lightcyan=0x7f040037;
+        public static final int lightcyan=0x7f04003a;
         /** 老花色 
          */
-        public static final int lightgoldenrodyellow=0x7f040024;
+        public static final int lightgoldenrodyellow=0x7f040027;
         /** 蓟色 
          */
-        public static final int lightgray=0x7f040040;
+        public static final int lightgray=0x7f040043;
         /** 中紫色 
          */
-        public static final int lightgreen=0x7f04005a;
+        public static final int lightgreen=0x7f04005c;
         /** 亮灰色 
          */
-        public static final int lightgrey=0x7f040041;
+        public static final int lightgrey=0x7f040044;
         /** 粉红色 
          */
-        public static final int lightpink=0x7f040017;
+        public static final int lightpink=0x7f04001a;
         /** 橙色 
          */
-        public static final int lightsalmon=0x7f040019;
+        public static final int lightsalmon=0x7f04001c;
         /** 森林绿 
          */
         public static final int lightseagreen=0x7f040083;
-        /** 紫罗兰蓝色 
-         */
-        public static final int lightskyblue=0x7f040060;
+        public static final int lightskyblue=0x7f040004;
         /** 中暗蓝色 
          */
         public static final int lightslategray=0x7f04006b;
@@ -257,10 +257,10 @@ public final class R {
         public static final int lightslategrey=0x7f04006c;
         /** 粉蓝色 
          */
-        public static final int lightsteelblue=0x7f04004e;
+        public static final int lightsteelblue=0x7f040051;
         /** 象牙色 
          */
-        public static final int lightyellow=0x7f040006;
+        public static final int lightyellow=0x7f040009;
         /** 春绿色
          */
         public static final int lime=0x7f040089;
@@ -269,10 +269,10 @@ public final class R {
         public static final int limegreen=0x7f04007e;
         /** 亮金黄色 
          */
-        public static final int linen=0x7f040025;
+        public static final int linen=0x7f040028;
         /** 紫红色 
          */
-        public static final int magenta=0x7f040021;
+        public static final int magenta=0x7f040024;
         /** 紫色 
          */
         public static final int maroon=0x7f040066;
@@ -284,10 +284,10 @@ public final class R {
         public static final int mediumblue=0x7f040092;
         /** 褐玫瑰红 
          */
-        public static final int mediumorchid=0x7f04004a;
+        public static final int mediumorchid=0x7f04004d;
         /** 暗紫罗兰色 
          */
-        public static final int mediumpurple=0x7f040059;
+        public static final int mediumpurple=0x7f04005b;
         /** 青绿色 
          */
         public static final int mediumseagreen=0x7f04007d;
@@ -302,31 +302,31 @@ public final class R {
         public static final int mediumturquoise=0x7f040078;
         /** 印第安红 
          */
-        public static final int mediumvioletred=0x7f040046;
+        public static final int mediumvioletred=0x7f040049;
         /** 闪兰色 
          */
         public static final int midnightblue=0x7f040085;
         /** 幽灵白 
          */
-        public static final int mintcream=0x7f040029;
+        public static final int mintcream=0x7f04002c;
         /** 白杏色 
          */
-        public static final int mistyrose=0x7f040010;
+        public static final int mistyrose=0x7f040013;
         /** 桔黄色 
          */
-        public static final int moccasin=0x7f040012;
+        public static final int moccasin=0x7f040015;
         /** 黑色 
          */
         public static final int my_grey=0x7f040096;
         /** 鹿皮色 
          */
-        public static final int navajowhite=0x7f040013;
+        public static final int navajowhite=0x7f040016;
         /** 暗蓝色 
          */
         public static final int navy=0x7f040094;
         /** 红色 
          */
-        public static final int oldlace=0x7f040023;
+        public static final int oldlace=0x7f040026;
         /** 灰色 
          */
         public static final int olive=0x7f040064;
@@ -335,79 +335,79 @@ public final class R {
         public static final int olivedrab=0x7f04006f;
         /** 亮粉红色
          */
-        public static final int orange=0x7f040018;
+        public static final int orange=0x7f04001b;
         /** 西红柿色 
          */
-        public static final int orangered=0x7f04001e;
+        public static final int orangered=0x7f040021;
         /** 金麒麟色 
          */
-        public static final int orchid=0x7f04003e;
+        public static final int orchid=0x7f040041;
         /** 亮珊瑚色 
          */
-        public static final int palegoldenrod=0x7f040033;
+        public static final int palegoldenrod=0x7f040036;
         /** 暗紫色
          */
-        public static final int palegreen=0x7f040057;
+        public static final int palegreen=0x7f040059;
         /** 亮钢兰色 
          */
-        public static final int paleturquoise=0x7f04004f;
+        public static final int paleturquoise=0x7f040052;
         /** 暗深红色 
          */
-        public static final int palevioletred=0x7f04003c;
+        public static final int palevioletred=0x7f04003f;
         /** 淡紫红 
          */
-        public static final int papayawhip=0x7f04000e;
+        public static final int papayawhip=0x7f040011;
         /** 纳瓦白 
          */
-        public static final int peachpuff=0x7f040014;
+        public static final int peachpuff=0x7f040017;
         /** 巧可力色 
          */
-        public static final int peru=0x7f040044;
+        public static final int peru=0x7f040047;
         /** 金色 
          */
-        public static final int pink=0x7f040016;
+        public static final int pink=0x7f040019;
         /** 实木色 
          */
-        public static final int plum=0x7f040039;
+        public static final int plum=0x7f04003c;
         /** 火砖色 
          */
-        public static final int powderblue=0x7f04004d;
+        public static final int powderblue=0x7f040050;
         /** 橄榄色 
          */
         public static final int purple=0x7f040065;
         /** 红紫色 
          */
-        public static final int red=0x7f040022;
+        public static final int red=0x7f040025;
         /** 暗黄褐色 
          */
-        public static final int rosybrown=0x7f040049;
+        public static final int rosybrown=0x7f04004c;
         /** 钢兰色 
          */
         public static final int royalblue=0x7f04007b;
         /** 暗海兰色 
          */
-        public static final int saddlebrown=0x7f04005c;
+        public static final int saddlebrown=0x7f04005e;
         /** 古董白 
          */
-        public static final int salmon=0x7f040027;
+        public static final int salmon=0x7f04002a;
         /** 浅黄色 
          */
-        public static final int sandybrown=0x7f04002d;
+        public static final int sandybrown=0x7f040030;
         /** 暗瓦灰色 
          */
         public static final int seagreen=0x7f040081;
         /** 米绸色 
          */
-        public static final int seashell=0x7f04000c;
+        public static final int seashell=0x7f04000f;
         /** 褐色 
          */
-        public static final int sienna=0x7f040055;
+        public static final int sienna=0x7f040057;
         /** 中紫罗兰色 
          */
-        public static final int silver=0x7f040047;
-        /** 亮天蓝色 
+        public static final int silver=0x7f04004a;
+        /** 亮蓝色 
          */
-        public static final int skyblue=0x7f040061;
+        public static final int skyblue=0x7f040006;
         /** 深绿褐色 
          */
         public static final int slateblue=0x7f040070;
@@ -419,7 +419,7 @@ public final class R {
         public static final int slategrey=0x7f04006e;
         /** 黄色 
          */
-        public static final int snow=0x7f040008;
+        public static final int snow=0x7f04000b;
         /** 青色 
          */
         public static final int springgreen=0x7f040088;
@@ -428,32 +428,34 @@ public final class R {
         public static final int steelblue=0x7f04007a;
         /** 亮灰色 
          */
-        public static final int tan=0x7f040042;
+        public static final int tan=0x7f040045;
         /** 暗青色 
          */
         public static final int teal=0x7f04008e;
         /** 淡紫色 
          */
-        public static final int thistle=0x7f04003f;
+        public static final int thistle=0x7f040042;
         /** 热粉红色 
          */
-        public static final int tomato=0x7f04001d;
+        public static final int tomato=0x7f040020;
         /** 皇家蓝 
          */
         public static final int turquoise=0x7f04007c;
         /** 苍麒麟色 
          */
-        public static final int violet=0x7f040034;
+        public static final int violet=0x7f040037;
         /** 米色 
          */
-        public static final int wheat=0x7f04002c;
-        public static final int white=0x7f040004;
+        public static final int wheat=0x7f04002f;
+        /** 天蓝色 
+         */
+        public static final int white=0x7f040007;
         /** 薄荷色 
          */
-        public static final int whitesmoke=0x7f04002a;
+        public static final int whitesmoke=0x7f04002d;
         /** 亮黄色
          */
-        public static final int yellow=0x7f040007;
+        public static final int yellow=0x7f04000a;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
