7c1898beae1fc84773a757b799d798f9adffbbe1
River <gngzhh1@qq.com>
2013-12-23 15:45:05 +0800

    去除api16

--- src/myViews/MyAgendaListAdapter.java
+++ src/myViews/MyAgendaListAdapter.java
@@ -27,11 +27,13 @@ import android.widget.TextView;
 
 public class MyAgendaListAdapter extends BaseAdapter {
 	private Activity act;
+	private Context c;
 	private String statics;
 	private LayoutInflater mInflater;
 	private List<Map<String, Object>> mData;
 
 	public MyAgendaListAdapter(Context context, List<Map<String, Object>> mData) {
+		c=context;
 		act=(Activity)context;
 		this.mInflater = LayoutInflater.from(context);
 		this.mData = mData;
@@ -55,7 +57,7 @@ public class MyAgendaListAdapter extends BaseAdapter {
 		return 0;
 	}
 
-	@SuppressLint("NewApi")
+//	@SuppressLint("NewApi")
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
@@ -80,40 +82,42 @@ public class MyAgendaListAdapter extends BaseAdapter {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		if(mData.get(position).get("isTitle").toString()=="1"){
-			RelativeLayout r = (RelativeLayout) convertView
-					.findViewById(R.id.agenda_layout);
+//			RelativeLayout r = (RelativeLayout) convertView
+//					.findViewById(R.id.agenda_layout);
 //			Drawable drawable = act.getResources().getDrawable(R.drawable.img01);
 //			r.setBackground(drawable);
-			r.setBackgroundColor(act.getResources().getColor(R.color.skyblue));
+			convertView.setBackgroundColor(c.getResources().getColor(R.color.Blue_Color));
 			holder.addBtn.setVisibility(View.GONE);
 			holder.time.setVisibility(View.GONE);
 			holder.address.setVisibility(View.GONE);
 			holder.viewBtn.setVisibility(View.GONE);
 			holder.title.setText((String) mData.get(position).get("title"));
-			holder.title.setTextColor(Color.BLACK);
+			holder.title.setTextColor(c.getResources().getColor(R.color.my_dark));
 		    TextPaint tp = holder.title.getPaint();   
 		    tp.setFakeBoldText(false); 
 //			holder.title.setText;
 //			holder.title.setTextSize(20);
 		}else{
-			RelativeLayout r = (RelativeLayout) convertView
-					.findViewById(R.id.agenda_layout);
-		    r.setBackgroundColor(Color.WHITE);
+//			RelativeLayout r = (RelativeLayout) convertView
+//					.findViewById(R.id.agenda_layout);
+			convertView.setBackgroundColor(Color.WHITE);
 		    holder.addBtn.setVisibility(View.VISIBLE);
 			holder.time.setVisibility(View.VISIBLE);
 			holder.address.setVisibility(View.VISIBLE);
 			holder.viewBtn.setVisibility(View.VISIBLE);
 //			holder.title.setTextSize(18);
-			holder.title.setTextColor(act.getResources().getColor(R.color.my_grey));
+			holder.title.setTextColor(c.getResources().getColor(R.color.my_grey));
 			TextPaint tp = holder.title.getPaint();   
 		    tp.setFakeBoldText(true); 
 			statics = mData.get(position).get("isSelected").toString();
 			if (statics == "0") {
-				Drawable drawable = act.getResources().getDrawable(R.drawable.ic_menu_add);
-				holder.addBtn.setBackground(drawable);
+				Drawable drawable = convertView.getResources().getDrawable(R.drawable.ic_menu_add);
+//				holder.addBtn.setBackground(drawable);
+				holder.addBtn.setBackgroundDrawable(drawable);
 			} else if(statics=="1"){
-				Drawable drawable = act.getResources().getDrawable(R.drawable.star3);
-				holder.addBtn.setBackground(drawable);
+				Drawable drawable = c.getResources().getDrawable(R.drawable.star3);
+//				holder.addBtn.setBackground();
+				holder.addBtn.setBackgroundDrawable(drawable);
 			}
 
 			holder.title.setText((String) mData.get(position).get("title"));
@@ -124,12 +128,14 @@ public class MyAgendaListAdapter extends BaseAdapter {
 				@Override
 				public void onClick(View v) {
 					if (statics == "0") {
-						Drawable drawable = act.getResources().getDrawable(R.drawable.star3);
-						holder.addBtn.setBackground(drawable);
+						Drawable drawable = c.getResources().getDrawable(R.drawable.star3);
+//						holder.addBtn.setBackground(drawable);
+						holder.addBtn.setBackgroundDrawable(drawable);
 						statics = "1";
 					} else if(statics=="1"){
-						Drawable drawable = act.getResources().getDrawable(R.drawable.ic_menu_add);
-						holder.addBtn.setBackground(drawable);
+						Drawable drawable = c.getResources().getDrawable(R.drawable.ic_menu_add);
+//						holder.addBtn.setBackground(drawable);
+						holder.addBtn.setBackgroundDrawable(drawable);
 						statics = "0";
 					}
 				}
